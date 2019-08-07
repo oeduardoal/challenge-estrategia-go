91c468c4458229d67179896e749a94f1c46d6a20
Eduardo Almeida <oeduardoal@gmail.com>
2019-08-06 21:29:56 -0300

    initial

--- controllers/user.go
+++ controllers/user.go
@@ -1,3 +1,3 @@
 package controllers
 
-func getUser() {}
+func eaoooooo() {}
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/oeduardoal/challenge-go-react/database"
 )
 
 func main() {
 	r := mux.NewRouter()
-	var a DB
-	r.HandleFunc("/api/v1/user/{user}", controllers).Methods("GET")
+	var a database.DB
+	var b controllers.eaoooooo
+	r.HandleFunc("/api/v1/user/{user}", eaoooooo).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
